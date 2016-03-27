c72d03b81513302fbeea858e53557b4b77a3d363
alejandro <a.quintero10@uniandes.edu.co>
2016-03-26 22:00:05 -0500

    my second commit

---
+++ server.js
@@ -0,0 +1,15 @@
+var http = require('http');
+var clientHtml = require('fs').readFileSync('client.html');
+var plainHttpServer = http.createServer(function (request,
+  response) {
+    response.writeHead(200, {'Content-type' : 'text/html'});
+    response.end(clientHtml);
+  }).listen(8080);
+
+var io = require('socket.io').listen(plainHttpServer);
+   io.sockets.on('connection', function (socket) {
+     socket.on('from', function (msg) {
+       if (msg === 'Hello') {
+         socket.emit('news','socket.io!');
+} });
+});
